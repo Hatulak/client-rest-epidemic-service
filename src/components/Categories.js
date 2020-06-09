c350595f3317d35cec4f389a310dfb00ea284c9f
hatul <przemek4651@o2.pl>
2020-06-09 18:38:32 +0200

    add categories file

---
+++ src/components/Categories.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getCategories } from "../actions/category";
+import { Link } from "react-router-dom";
+import moment from "moment";
+
+class Categories extends Component {
+
+  static propTypes = {
+    categories: PropTypes.array.isRequired,
+    getCategories: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    if (this.props.categories) {
+      this.props.getCategories();
+      console.log(this.props.categories);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Categories:</h1>
+        <table className="table table-striped" datapagesize={5}>
+          <thead>
+            <tr>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.categories.map((cat) => (
+              <tr key={cat.id}>
+                <td>{cat.name}</td>
+                <td>
+                  <Link
+                    to={`/editCategory/${cat.id}`}
+                    className={"btn btn-primary"}
+                  >
+                    Edit
+                  </Link> 
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+    categories: state.category.categories
+});
+export default connect(mapStateToProps, {
+  getCategories,
+})(Categories);
